45e23217365a3bc279b974d5aef6f64901ecad6d
James Mills <prologic@shortcircuit.net.au>
2014-11-15 23:04:33 +1000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM crux/python:onbuild
+MAINTAINER James Mills, prologic at shortcircuit dot net dot au
+
+EXPOSE 80
+
+CMD ["hello"]
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup
+
+
+setup(
+    name="hello",
+    version="0.0.1",
+    description="Hello World!",
+    scripts=["hello"],
+)
